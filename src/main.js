1317b67c5ff1acb30d388ec977db352e013e198b
Ada Krupa <adriannakrupa01@gmail.com>
2022-10-10 15:30:32 +0200

    Fix added vue-toastification

--- src/main.js
+++ src/main.js
@@ -3,8 +3,10 @@ import App from '@/App.vue';
 import router from '@/router';
 import vuetify from '@/plugins/vuetify';
 import { loadFonts } from '@/plugins/webfontloader';
+import Toast from 'vue-toastification';
+import 'vue-toastification/dist/index.css';
 import '@/styles/global.scss';
 
 loadFonts();
 
-createApp(App).use(router).use(vuetify).mount('#app');
+createApp(App).use(router).use(vuetify).use(Toast).mount('#app');
